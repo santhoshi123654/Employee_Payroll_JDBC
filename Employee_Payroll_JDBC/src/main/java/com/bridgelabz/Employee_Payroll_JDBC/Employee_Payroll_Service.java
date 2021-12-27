b0e23d1ae4deb65e06a407a12d6cb6ab57b472c2
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-27 17:04:51 +0530

    Added:Update Salary Using Prepared statement

---
+++ Employee_Payroll_JDBC/src/main/java/com/bridgelabz/Employee_Payroll_JDBC/Employee_Payroll_Service.java
@@ -0,0 +1,95 @@
+package com.bridgelabz.Employee_Payroll_JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Employee_Payroll_Service {
+	
+	Connection connection = null;
+	Statement statement = null;
+	PreparedStatement preparedStatement = null;	
+	ResultSet resultSet = null;
+
+	public static List<EmployeePayroll> list = new ArrayList<EmployeePayroll>();
+	EmployeePayroll employeePayroll = new EmployeePayroll();
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	public PreparedStatement getConnection() {
+
+		String jdbcURL = "jdbc:mysql://localhost:3306/bridgelabzDemo?useSSL=false";
+		String userName = "root";
+		String password = "Santhu@123";
+		System.out.println("connecting to database : " + jdbcURL);
+		try {
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection is successfull " + connection);
+			
+			preparedStatement = connection.prepareStatement("select * from employee_payroll where EmpName='Santhoshi'");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return preparedStatement;
+	}
+	
+	public int updateData(PreparedStatement preparedStatement) {
+
+		System.out.println("update query : ");
+		String updateString = scanner.nextLine();
+		int count = 0;
+		try {
+			count = preparedStatement.executeUpdate(updateString);
+			System.out.println("successfully updated");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
+
+	public ResultSet retrieveData(Statement statement) {
+
+		System.out.println("enter Select query : ");
+		String selectString = scanner.nextLine();
+		try {
+			resultSet = statement.executeQuery(selectString);
+			System.out.println("retrived successfully");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultSet;
+	}
+
+	public void printRetrivedData(ResultSet resultSet) throws PatternMatchException {
+		try {
+			while (resultSet.next()) {
+
+				employeePayroll.setEmpId(resultSet.getInt("EmpId"));
+				employeePayroll.setEmpName(resultSet.getString("EmpName"));
+				employeePayroll.setPhone_Number(resultSet.getLong("Phone_Number"));
+				employeePayroll.setAddress(resultSet.getString("Address"));
+				employeePayroll.setDepartment(resultSet.getString("Department"));
+				employeePayroll.setGender(resultSet.getString("gender"));
+				employeePayroll.setBasicPay(resultSet.getDouble("BasicPay"));
+				employeePayroll.setDeduction(resultSet.getString("Deduction"));
+				employeePayroll.setTaxablePay(resultSet.getString("TaxablePay"));
+				employeePayroll.setIncomeTax(resultSet.getString("IncomeTax"));
+				employeePayroll.setNetPay(resultSet.getString("NetPay"));
+				employeePayroll.setStartDate(resultSet.getDate("StartDate"));
+				
+				list.add(employeePayroll);
+			}
+		}
+		catch (Exception e) {
+
+			throw new PatternMatchException("Pattern is Mismatched -The database should be checked ");
+		}
+		System.out.println(list);
+	}
+}
